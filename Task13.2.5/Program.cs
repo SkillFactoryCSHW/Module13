ca8385e67fd525f44076fab2a52be8cd0af7e41c
Bochanikita <bochanikita@yandex.ru>
2022-12-11 18:23:24 +0300

    Add project files.

---
+++ Task13.2.5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+
+var months = new[]
+{
+    "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+};
+
+var numbers = new[]
+{
+    1,2,3,4,5,6,7,8,9,10,11,12
+};
+
+var combinedList = new ArrayList();
+
+foreach (var number in numbers)
+{
+    combinedList.Add(months[number - 1]);
+
+    combinedList.Add(number);
+}
+foreach (var number in combinedList)
+{
+    Console.Write(number + " ");
+}
