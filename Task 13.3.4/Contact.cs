e64858f87d6b45151fdcab36759141ad878d5a1e
Bochanikita <bochanikita@yandex.ru>
2022-12-12 22:31:01 +0300

    Add task 13.3.4

---
+++ Task 13.3.4/Contact.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Contact
+{
+    string name;
+    long phoneNumber;
+    string email;
+
+    public Contact(string name, long phoneNumber, string email)
+    {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public string Name { get { return name; }}
+    public long PhoneNumber { get { return phoneNumber; }}
+    public string Email { get { return email; }}
+}
---
+++ Task 13.3.4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+string name;
+long number;
+string email;
+
+var phoneBook = new List<Contact>();
+phoneBook.Add(new Contact("Mike", 100, "@mail"));
+phoneBook.Add(new Contact("Loh", 200, "@mail"));
+
+Console.WriteLine("Enter the name");
+name = Console.ReadLine();
+Console.WriteLine("Enter the number");
+number = Convert.ToInt64(Console.ReadLine());
+Console.WriteLine("Enter the email");
+email = Console.ReadLine();
+
+Contact contact = new Contact(name, number, email);
+AddUnique(contact, ref phoneBook);
+
+foreach (var item in phoneBook)
+{
+    Console.Write("Name: " + item.Name + ", " + "Number: " + item.PhoneNumber + ", " + "Email: " + item.Email);
+	Console.WriteLine();
+}
+
+
+static List<Contact> AddUnique(Contact contact,ref List<Contact> phoneBook)
+{
+	foreach (var item in phoneBook)
+	{
+		if (item.Name == contact.Name)
+		{
+            Console.WriteLine("Already exist");
+			break;
+        }
+		else
+		{
+			phoneBook.Add(contact);
+            break;
+        }
+	}
+	return phoneBook;
+}
