059ca48a54c1671ef6478e12474789b12ccc85fa
Bochanikita <bochanikita@yandex.ru>
2022-12-19 20:55:57 +0300

    Add Task 13.6.2

---
+++ Task13.6.2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+string path = @"D:\Test\Text1.txt";
+StreamReader reader = new StreamReader(path);
+string text = reader.ReadToEnd();
+
+var noPunctuationText = new string(text.Where(c => !char.IsPunctuation(c)).ToArray());
+
+var orderedWords = noPunctuationText
+  .Split(' ')
+  .GroupBy(x => x)
+  .Select(x => new {
+      KeyField = x.Key,
+      Count = x.Count()
+  })
+  .OrderByDescending(x => x.Count)
+  .Take(10);
+
+foreach (var word in orderedWords)
+{
+    Console.WriteLine(word);
+}
